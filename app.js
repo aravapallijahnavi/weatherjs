7ca971a2701dce6729538bb4da84686ac7cae2b8
aravapallijahnavi <110587130+aravapallijahnavi@users.noreply.github.com>
2024-05-04 20:21:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  // Serve the HTML file
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, '12.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else {
+    // Handle other requests
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
